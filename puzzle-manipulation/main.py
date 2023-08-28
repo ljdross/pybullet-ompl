01cac8ccc9e1e249ae8feb4e387051b53dd96be6
ljdross <91616509+ljdross@users.noreply.github.com>
2023-08-28 23:14:39 +0200

    feat: add action sequence

--- puzzle-manipulation/main.py
+++ puzzle-manipulation/main.py
@@ -1,5 +1,6 @@
 from src.manipulation import Manipulation
 from src.configuration import Configuration
+from src.action import Action
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
         merged_world_output_path="/home/le/ba/pybullet-ompl/models/kuka_iiwa/merged_world.urdf",
         robot_start_state=(-1, -1, 0, 0, 0, -1, 0, 1.5, 0),
         puzzle_start_state=(0, 0, 0, 0),
-        action_sequence=(),
+        action_sequence=(Action(3, 0.5), Action(2, 0.5), Action(1, 0.5), Action(0, 0.5)),
         ompl_planner="BITstar",
         step_size=0.002
     )
