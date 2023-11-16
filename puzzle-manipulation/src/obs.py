1a358a80abf90f3cd0b023695c98f5018293cfc5
ljdross <91616509+ljdross@users.noreply.github.com>
2023-11-16 21:55:49 +0100

    fix: do not throw if no connection to obs

--- puzzle-manipulation/src/obs.py
+++ puzzle-manipulation/src/obs.py
@@ -3,12 +3,18 @@ import obsws_python as obs
 
 
 def start_recording():
-    cl = obs.ReqClient()
-    cl.start_record()
+    try:
+        cl = obs.ReqClient()
+        cl.start_record()
+    except:
+        print("Cannot start recording because of no connection to OBS websocket")
 
 
 def stop_recording():
-    cl = obs.ReqClient()
-    cl.stop_record()
-    time.sleep(10)
+    try:
+        cl = obs.ReqClient()
+        cl.stop_record()
+        time.sleep(10)
+    except:
+        print("Cannot stop recording because of no connection to OBS websocket")
 
