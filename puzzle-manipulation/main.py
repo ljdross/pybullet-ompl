d23ed5492fc069808d82bd188e599c40c71275c8
ljdross <91616509+ljdross@users.noreply.github.com>
2023-12-11 18:28:45 +0100

    feat: uncommented config files in main for convenience

--- puzzle-manipulation/main.py
+++ puzzle-manipulation/main.py
@@ -6,7 +6,12 @@ from src import obs
 
 
 def main():
-    config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/simple_sliders_config.json'
+    # config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/simple_sliders.json'
+    # config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/grid_world3.json'
+    # config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/continuous_space3.json'
+    # config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/lockbox2017.json'
+    # config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/lockbox_random3.json'
+    config_json = sys.argv[1] if len(sys.argv) > 1 else './configurations/rooms0.json'
     defaults_json = sys.argv[2] if len(sys.argv) > 2 else './configurations/defaults.json'
 
     config_parser = ConfigParser(config_json, defaults_json)
